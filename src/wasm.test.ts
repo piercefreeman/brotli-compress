0974c1b89bc96fba65c83e47dc79eac02b8f09d0
Pierce Freeman <piercefreeman@gmail.com>
2023-09-18 17:04:19 -0700

    Add test case to verify iteration success

--- src/wasm.test.ts
+++ src/wasm.test.ts
@@ -11,3 +11,13 @@ it('Compresses and decompresses a string', async () => {
   const decompressed = await decompress(compressed)
   expect(Buffer.from(decompressed).toString()).toStrictEqual(TEST_INPUT)
 })
+
+it("Doesn't crash on high iterations", async () => {
+  // Example case from https://github.com/httptoolkit/brotli-wasm/issues/16
+  for (let i = 0; i < 1000; i++) {
+    const testInput = Buffer.from(TEST_INPUT)
+    const compressed = await compress(testInput)
+    const decompressed = await decompress(compressed)
+    expect(Buffer.from(decompressed).toString()).toStrictEqual(TEST_INPUT)  
+  }
+})
